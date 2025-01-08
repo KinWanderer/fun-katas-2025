09ea6f73e1c8afc4c7a3524624a7a8b10b8465cc
KinWanderer <joss.sessions@gmail.com>
2025-01-08 15:18:52 +0000

    calculate_divisors with ValueError tests

--- src/calculate_divisors.py
+++ src/calculate_divisors.py
@@ -1,7 +1,13 @@
-# Function to calculate the divisors of a given number
-def calculate_divisors(number):
-    divisors = [0]
+# a func with SUMS all the MULTIPLES of 3 and 5 
+# belew an input number
+
+def sum_divisors(number):
+    if not isinstance(number, int) or number <= 0:
+        raise ValueError("Input number must be a positive integer")
+    
+    divisors = []
     for i in range(1, number):
-        if (number % i)==0:
+        if i % 3 == 0 or i % 5 == 0:
             divisors.append(i)
+
     return sum(divisors)
--- test/test_calculate_divisors.py
+++ test/test_calculate_divisors.py
@@ -1,25 +1,82 @@
-from src.calculate_divisors import calculate_divisors
+from src.calculate_divisors import sum_divisors
 
+class TestCalculateDivisors:
 # 1. return 0 if input number is 1
-def test_calculate_divisor_of_1_to_return_zero():
-    # Assemble
-    input_number = 1
+    def test_calculate_divisor_of_1_to_return_zero(self):
+        # Assemble
+        input_number = 1
 
-    # Act
-    output = calculate_divisors(input_number)
-    expected_output = []
+        # Act
+        output = sum_divisors(input_number)
+        expected_output = 0
 
-    # Assert
-    assert output == expected_output
+        # Assert
+        assert output == expected_output
 
-# 2. return 3 with input number 5
-def test_calculate_divisor_of_6_to_return_3():
-    # Assemble
-    input_number = 5
+    # 2. return 3 with input number 5
+    def test_calculate_divisor_of_6_to_return_3(self):
+        # Assemble
+        input_number = 5
 
-    # Act
-    output = calculate_divisors(input_number)
-    expected_output = [3]
+        # Act
+        output = sum_divisors(input_number)
+        expected_output = 3
 
-    # Assert
-    assert output == expected_output
+        # Assert
+        assert output == expected_output
+
+    # 3. return 8 with input number 6
+    def test_calculate_divisor_of_8_to_return_6(self):
+        # Assemble
+        input_number = 6
+
+        # Act
+        output = sum_divisors(input_number)
+        expected_output = 8
+
+        # Assert
+        assert output == expected_output
+
+    # 4. return 23 with input number 10
+    def test_calculate_divisor_of_10_to_return_23(self):
+        # Assemble
+        input_number = 10
+
+        # Act
+        output = sum_divisors(input_number)
+        expected_output = 23
+
+        # Assert
+        assert output == expected_output
+
+    # 5 return 33 with input number 12
+
+    def test_calculate_divisor_of_12_to_return_33(self):
+         # Assemble
+        input_number = 12
+
+        # Act
+        output = sum_divisors(input_number)
+        expected_output = 33
+
+        # Assert
+        assert output == expected_output
+
+class TestExpectedFails:
+    def test_returns_ValueError_if_input_is_negative(self):
+        try: 
+            sum_divisors(-1)
+        except ValueError as e:
+            assert str(e) == "Input number must be a positive integer"
+    
+    def test_returns_ValueError_if_input_is_float(self):
+        try:
+            sum_divisors(3.7)
+        except ValueError as e:
+            assert str(e) == "Input number must be a positive integer"
+        
+    def test_returns_ValueError_if_input_is_string(self):
+        try:
+            sum_divisors("Three")
+        except ValueError as e:
+            assert str(e) == "Input number must be a positive integer"
