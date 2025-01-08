7d20cfc13bd03877f0553f51c70e7f8471c578ee
KinWanderer <joss.sessions@gmail.com>
2025-01-08 15:43:37 +0000

    remote student setup

---
+++ src/remote_students.py
@@ -0,0 +1,19 @@
+""" The challenge is to implement a function that updates 
+missing location info for students attending Northcoders.
+
+
+Your function should return a new list, 
+where all dictionaries that were missing a location 
+have been updated so that they now have a location property 
+with a value of "remote"."""
+
+
+
+# a func APPENDS STRING(location) to a dict 
+# call list dict 
+# each dict belongs to a NC student 
+# some strings have location, some don't 
+# return a new LIST with ALL missing locations updated to remote  
+
+def remote_locations():
+    pass 
---
+++ test/test_remote_students.py
@@ -0,0 +1,19 @@
+from src.remote_students import remote_locations
+"""This function must be a PURE FUNCTION, 
+i.e. it should have no side effects. 
+Your test suite should reflect this."""
+
+
+# test setup 
+# test call list dict 
+# test returns missing location 
+# test returns new list 
+
+
+class TestBuildOfRemoteStudents():
+    def test_setup_of_files(self):
+        assert remote_locations 
+
+    def test_call_lst_dict(self):
+
+        assert remote_locations 
