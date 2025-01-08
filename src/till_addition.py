17131253d7208a85c2e00e6cbca3db8964eece98
KinWanderer <joss.sessions@gmail.com>
2025-01-08 17:15:34 +0000

    resolved till_additions

--- src/till_addition.py
+++ src/till_addition.py
@@ -22,3 +22,39 @@ till = {
 }
 
 # a func SUMS till to return a total STRING
+# inilise SUM
+# iterate through string 
+# define denotation * number of coins
+# update  f' £total format setter
+
+def end_of_day(till):
+    total = 0 
+    for money, count_of in till.items():
+        if money == "1p":
+            total += 0.01 * count_of
+        elif money == "2p":
+            total += 0.02 * count_of
+        elif money == "5p":
+            total += 0.05 * count_of
+        elif money == "10p":
+            total += 0.10 * count_of
+        elif money == "20p":
+            total += 0.20 * count_of
+        elif money == "50p":
+            total += 0.50 * count_of
+        elif money == "£1":
+            total += 1.00 * count_of
+        elif money == "£2":
+            total += 2.00 * count_of
+        elif money == "£5":
+            total += 5.00 * count_of
+        elif money == "£10":
+            total += 10.00 * count_of
+        elif money == "£20":
+            total += 20.00 * count_of
+        elif money == "£50":
+            total += 50.00 * count_of
+    return f"£{total:.2f}"
+        
+
+     
--- test/test_till_addition.py
+++ test/test_till_addition.py
@@ -1,4 +1,6 @@
-""" till_addition({ "1p": 1, "2p": 1 })
+from src.till_addition import end_of_day
+
+""" till_addition({ "1p": 1, "2p": 1 }
 # should return "£0.03"
 
 till_addition({ "1p": 1, "2p": 1, "5p": 1, "10p": 1, "20p": 1 })
@@ -6,3 +8,18 @@ till_addition({ "1p": 1, "2p": 1, "5p": 1, "10p": 1, "20p": 1 })
 
 till_addition({ "5p": 1, "10p": 1, "20p": 1, "50p": 1, "£1": 1 })
 # should return "£1.85" """
+
+class TestEndOfDay():
+    def test_till_addition_small(self):
+        assert end_of_day({ "1p": 1, "2p": 1 }) == "£0.03"
+        assert end_of_day({ "1p": 1, "2p": 1, "5p": 1, "10p": 1, "20p": 1 }) == "£0.38"
+        assert end_of_day({ "5p": 1, "10p": 1, "20p": 1, "50p": 1, "£1": 1 }) == "£1.85"
+
+    def test_till_addition_large(self):
+        assert end_of_day({"£20": 4, "£5": 1}) == "£85.00"
+        assert end_of_day({"£10": 11, "£50": 1}) == "£160.00"
+
+    def test_unepected_item_in_bagging_area(self):
+        assert end_of_day({"p1": 55}) == "£0.00"
+        assert end_of_day({}) == "£0.00"
+        
